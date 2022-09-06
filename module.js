1492696acc5c41e06e4ff7a6fa6b9e32e9f09876
Chidubem Akpu <stanleychidubemakpu@gmail.com>
2022-09-06 16:09:44 +0100

    Time

--- module.js
+++ module.js
@@ -1,28 +1,56 @@
-let theCarousel = document.querySelector('.carousel-inner');
-let pos = 0;
-let theSize = 0;
-function carouselFunc() {
-    let theScrollLeaf = document.querySelectorAll('.example');
-    theCarousel.style.transform = `translateX(${theSize}px)`
-    theSize -= 200;
-    theCarousel.addEventListener('transitionend', function handler() {
-        theCarousel.removeEventListener('transitionend', handler)
-        new Promise(resolve => {
-            setTimeout(() => {
-                if (pos == 7){
-                    pos = 0;
-                }
-                console.log(pos)
-                let theRealWidth = parseInt(getComputedStyle(theCarousel).width);
-                let theClone = theScrollLeaf[pos].cloneNode(true);
-                pos++;
-                theCarousel.style.width =  theRealWidth + 250 + 'px';
-                theCarousel.append(theClone);
-                resolve(carouselFunc)}, 3000);
-        }).then(a => a())
-    })
+
+// myDom.addEventListener('pointerdown', thePointerListener);
+// function thePointerListener(event){
+//     if (event.target.className == 'container') return;
+//     let theDom = event.target.closest('.to-learn-events');
+//     theDom.style.position = 'relative';
+//     theDom.style.zIndex = 1000;
+    
+//     theDom.addEventListener('mousemove', moveEl) ;
+//      function moveEl(el) {
+//         let theDomReal = getComputedStyle(theDom);
+//         theDom.style.top = parseFloat(theDomReal.top)  + 1 + 'px';
+//         theDom.style.display= 'none'
+//         let theBelow = document.elementFromPoint(el.clientX, el.clientY);
+//         if (theBelow.className == 'container') return;
+//         console.log(theBelow.id)
+//         theBelow.insertAdjacentElement('afterend', theDom);
+//         theDom.style.display = 'grid'
+//         theDom.mouseup = function(e) {
+//             theDom.removeEventListener('mousemove', moveEl);
+//             theDom.mouseup = null;
+//         }
+        
+//     }
+
+
+// }
+let timeModule = document.querySelectorAll('.each-Item');
+console.log(timeModule)
+function theRealTime(){
+    let date = new Date();
+    let [hours, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()]
+    if (hours > 12) {
+           let myHours =  hours - 12;
+           timeModule[3].innerHTML = 'PM'
+           if (myHours < 10){
+            timeModule[0].innerHTML = '0' + myHours;
+           } else {
+            timeModule[0].innerHTML = myHours;
+           }
+    if (minutes < 10) {
+        timeModule[1].innerHTML = '0' + minutes;
+    } else {
+        timeModule[1].innerHTML = minutes;
+    }
+    if (seconds < 10) {
+        timeModule[2].innerHTML = '0' + seconds;
+    } else {
+        timeModule[2].innerHTML = seconds;
+    }
+
+    } else {
+        timeModule[3].innerHTML = 'AM'
+    }
 }
-carouselFunc();
-
-
-
+setInterval(theRealTime, 1000)
--- script.js
+++ script.js
@@ -1,54 +1,80 @@
-// let myChanger= document.querySelector('p');
-// let myBox = document.querySelector('div');
-// let myLoader = document.querySelector('section')
+// // let myChanger= document.querySelector('p');
+// // let myBox = document.querySelector('div');
+// // let myLoader = document.querySelector('section')
 
-// myChanger.addEventListener('click', birthdayBox);
+// // myChanger.addEventListener('click', birthdayBox);
 
-// function birthdayBox() {
-//   myLoader.classList.toggle('remove-loader')
-//   myBox.classList.toggle('display-class');
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve(birthdayBox);
-//     }, 6000)
-//   }).then(getit => getit());
+// // function birthdayBox() {
+// //   myLoader.classList.toggle('remove-loader')
+// //   myBox.classList.toggle('display-class');
+// //   return new Promise((resolve, reject) => {
+// //     setTimeout(() => {
+// //       resolve(birthdayBox);
+// //     }, 6000)
+// //   }).then(getit => getit());
+// // }
+
+
+
+// // // // // 'use strict';
+
+// // // // // const range = {
+// // // // //     from: 1,
+// // // // //     to: 20,
+
+// // // // //     [Symbol.iterator]() {
+// // // // //         this.current = this.from;
+// // // // //         return this;
+// // // // //     },
+
+// // // // //     next() {
+// // // // //         if (this.current <= this.to) {
+// // // // //             return {done: false, value: this.current++}
+// // // // //         } else {
+// // // // //             return {done: true}
+// // // // //         }
+// // // // //     }
+// // // // // }
+
+// // // // // for (let eachEl of range) {
+// // // // //     alert(eachEl)
+// // // // // }
+
+
+
+// // // // let myStr = 'HelloUser';
+
+// // // // let iterator = myStr[Symbol.iterator]();
+// // // // while(true) {
+// // // //     let result = iterator.next();
+// // // //     if (result.done) break;
+// // // //     alert(result.value)
+// // // // }
+
+// // // // addded stuff
+// let theCarousel = document.querySelector('.carousel-inner');
+// let pos = 0;
+// let theSize = 0;
+// function carouselFunc() {
+//     let theScrollLeaf = document.querySelectorAll('.example');
+//     theCarousel.style.transform = `translateX(${theSize}px)`
+//     theSize -= 200;
+//     theCarousel.addEventListener('transitionend', function handler() {
+//         theCarousel.removeEventListener('transitionend', handler)
+//         new Promise(resolve => {
+//             setTimeout(() => {
+//                 if (pos == 7){
+                    
+//                     pos = 0;
+//                 }
+//                 let theRealWidth = parseInt(getComputedStyle(theCarousel).width);
+//                 let theClone = theScrollLeaf[pos].cloneNode(true);
+//                 pos++;
+//                 theCarousel.style.width =  theRealWidth + 250 + 'px';
+//                 theCarousel.append(theClone);
+//                 resolve(carouselFunc)}, 3000);
+//         }).then(a => a())
+//     })
 // }
 
-
-
-// // // // 'use strict';
-
-// // // // const range = {
-// // // //     from: 1,
-// // // //     to: 20,
-
-// // // //     [Symbol.iterator]() {
-// // // //         this.current = this.from;
-// // // //         return this;
-// // // //     },
-
-// // // //     next() {
-// // // //         if (this.current <= this.to) {
-// // // //             return {done: false, value: this.current++}
-// // // //         } else {
-// // // //             return {done: true}
-// // // //         }
-// // // //     }
-// // // // }
-
-// // // // for (let eachEl of range) {
-// // // //     alert(eachEl)
-// // // // }
-
-
-
-// // // let myStr = 'HelloUser';
-
-// // // let iterator = myStr[Symbol.iterator]();
-// // // while(true) {
-// // //     let result = iterator.next();
-// // //     if (result.done) break;
-// // //     alert(result.value)
-// // // }
-
-// // // addded stuff
+// carouselFunc();
