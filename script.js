856c9fa04dc880896053640b050493ec2f553959
Chidubem Akpu <stanleychidubemakpu@gmail.com>
2022-09-06 20:37:55 +0100

    Added password generator

--- script.js
+++ script.js
@@ -1,80 +1,31 @@
-// // let myChanger= document.querySelector('p');
-// // let myBox = document.querySelector('div');
-// // let myLoader = document.querySelector('section')
+const actionButtons = document.querySelector('.buttons');
+const displayRegion = document.querySelector('.gen-input');
+const myForm = document.querySelector('form');
+const myAction= document.querySelector('.view-action')
+actionButtons.addEventListener('click', generatePassWord);
+function generatePassWord(event) {
+    if (!event.target.value) return;
 
-// // myChanger.addEventListener('click', birthdayBox);
+    if (event.target.value == 'Generate') {
+        getPassWord()
+        return;
+    }
+    navigator.clipboard.writeText(displayRegion.value);
+    myAction.style.opacity = 1;
+    setTimeout(() => myAction.style.opacity = '', 1000);
+    displayRegion.value = '';
 
-// // function birthdayBox() {
-// //   myLoader.classList.toggle('remove-loader')
-// //   myBox.classList.toggle('display-class');
-// //   return new Promise((resolve, reject) => {
-// //     setTimeout(() => {
-// //       resolve(birthdayBox);
-// //     }, 6000)
-// //   }).then(getit => getit());
-// // }
+}
 
+function getPassWord() {
+    let thePassword = ''
+    let letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    for (let i = 0; i<=8; i++) {
+        let pos = Math.ceil(Math.random() * letters.length);
+        thePassword += letters.charAt(pos)
+    }
+    displayRegion.value = thePassword;
 
-
-// // // // // 'use strict';
-
-// // // // // const range = {
-// // // // //     from: 1,
-// // // // //     to: 20,
-
-// // // // //     [Symbol.iterator]() {
-// // // // //         this.current = this.from;
-// // // // //         return this;
-// // // // //     },
-
-// // // // //     next() {
-// // // // //         if (this.current <= this.to) {
-// // // // //             return {done: false, value: this.current++}
-// // // // //         } else {
-// // // // //             return {done: true}
-// // // // //         }
-// // // // //     }
-// // // // // }
-
-// // // // // for (let eachEl of range) {
-// // // // //     alert(eachEl)
-// // // // // }
+}
 
 
-
-// // // // let myStr = 'HelloUser';
-
-// // // // let iterator = myStr[Symbol.iterator]();
-// // // // while(true) {
-// // // //     let result = iterator.next();
-// // // //     if (result.done) break;
-// // // //     alert(result.value)
-// // // // }
-
-// // // // addded stuff
-// let theCarousel = document.querySelector('.carousel-inner');
-// let pos = 0;
-// let theSize = 0;
-// function carouselFunc() {
-//     let theScrollLeaf = document.querySelectorAll('.example');
-//     theCarousel.style.transform = `translateX(${theSize}px)`
-//     theSize -= 200;
-//     theCarousel.addEventListener('transitionend', function handler() {
-//         theCarousel.removeEventListener('transitionend', handler)
-//         new Promise(resolve => {
-//             setTimeout(() => {
-//                 if (pos == 7){
-                    
-//                     pos = 0;
-//                 }
-//                 let theRealWidth = parseInt(getComputedStyle(theCarousel).width);
-//                 let theClone = theScrollLeaf[pos].cloneNode(true);
-//                 pos++;
-//                 theCarousel.style.width =  theRealWidth + 250 + 'px';
-//                 theCarousel.append(theClone);
-//                 resolve(carouselFunc)}, 3000);
-//         }).then(a => a())
-//     })
-// }
-
-// carouselFunc();
